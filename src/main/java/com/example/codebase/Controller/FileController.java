d9650128ca2d27114fe5720d4de56a310542748b
sleeg00 <slee000220@gmail.com>
2023-02-09 05:24:34 +0900

    feat:getDir 수정

--- src/main/java/com/example/codebase/Controller/FileController.java
+++ src/main/java/com/example/codebase/Controller/FileController.java
@@ -7,7 +7,6 @@ import com.example.codebase.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/example/codebase/Service/FileService.java
+++ src/main/java/com/example/codebase/Service/FileService.java
@@ -29,7 +29,7 @@ import java.util.Map;
 @Service
 public class FileService {
 
-    public static String rootPath = "/mos_file/"; //root Path
+    public static String rootPath = "/Users/leeseonghyeon/Desktop/Mega/"; //root Path
 
 
 
@@ -112,28 +112,19 @@ public class FileService {
                         .code(HttpStatus.OK.value())
                         .httpStatus(HttpStatus.OK)
                         .message("파일 이름 변경 성공")
-                        .accessToken("")
-                        .refreshToken("")
-                        .result(null)
-                        .count(1).build();
+                        .build();
             else
                 basicResponse = BasicResponse.builder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .httpStatus(HttpStatus.BAD_REQUEST)
                         .message("파일 이름 변경 실패")
-                        .accessToken("")
-                        .refreshToken("")
-                        .result(null)
-                        .count(1).build();
+                        .build();
         } else {
             basicResponse = BasicResponse.builder()
                     .code(HttpStatus.BAD_REQUEST.value())
                     .httpStatus(HttpStatus.BAD_REQUEST)
                     .message("파일 이름 변경 실패")
-                    .accessToken("")
-                    .refreshToken("")
-                    .result(null)
-                    .count(1).build();
+                    .build();
         }
         return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
     }
@@ -151,34 +142,25 @@ public class FileService {
                         .code(HttpStatus.OK.value())
                         .httpStatus(HttpStatus.OK)
                         .message("폴더 이름 변경 성공")
-                        .accessToken("")
-                        .refreshToken("")
-                        .result(null)
-                        .count(1).build();
+                        .build();
             else
                 basicResponse = BasicResponse.builder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .httpStatus(HttpStatus.BAD_REQUEST)
                         .message("폴더 이름 변경 실패")
-                        .accessToken("")
-                        .refreshToken("")
-                        .result(null)
-                        .count(1).build();
+                        .build();
         } else {
             basicResponse = BasicResponse.builder()
                     .code(HttpStatus.BAD_REQUEST.value())
                     .httpStatus(HttpStatus.BAD_REQUEST)
                     .message("폴더 이름 변경 실패")
-                    .accessToken("")
-                    .refreshToken("")
-                    .result(null)
-                    .count(1).build();
+                    .build();
         }
         return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
     }
 
     public ResponseEntity<BasicResponse> getDir(String member_id, String dir) {    //폴더 구조 가져오기
-            File newDir = new File(rootPath +member_id+"/"+ dir);
+            File newDir = new File(rootPath + dir);
 
             File[] fileList = newDir.listFiles();
 
@@ -190,30 +172,38 @@ public class FileService {
             for (File file : fileList) {
                 getDirectory insert = new getDirectory();
                 if (file.exists()) {
-
                     String fileName = file.getName();
 
-                    if(file.isFile()) {
-                        String[] name = fileName.split(".");
-                        if(name.length!=0) {
-                            insert = getDirectory.builder()
-                                    .isDir(false)
-                                    .name(name[0])
-                                    .ext(name[1])
-                                    .build();
-                            list.add(insert);
+                    if (file.isFile()) {
+                        int cnt=0;
+                        char []ch = fileName.toCharArray();
+                        for(int i= ch.length-1; i>=0; i--) {
+                            if(ch[i]=='.')
+                                cnt=i;
                         }
-                    }
-                    else if(file.isDirectory()){
+
+                        char []ex = new char[ch.length-(cnt+1)];
+                        for(int i=cnt+1; i<ch.length; i++){
+                            ex[i-cnt-1]=ch[i];
+                        }
+
+                        insert = getDirectory.builder()
+                                .isDir(false)
+                                .name(fileName)
+                                .ext(String.valueOf(ex))
+                                .build();
+                        list.add(insert);
+                    } else if(file.isDirectory()){
                         insert = getDirectory.builder()
                                 .isDir(true)
                                 .name(fileName)
                                 .ext("Directory")
                                 .build();
                         list.add(insert);
-                        }
                     }
                 }
+            }
+
 
             basicResponse = BasicResponse.builder()
                     .code(HttpStatus.OK.value())
@@ -227,7 +217,7 @@ public class FileService {
     //디렉토리 삭제(하위폴더,파일 모두 삭제됨)
 
     public ResponseEntity removeDir(String member_id, String dir, String rm) {
-        File rm_dir = new File(rootPath +member_id+"/"+ dir);
+        File rm_dir = new File(rootPath +member_id+ dir);
         String response = new String();
 
         if (rm_dir.exists() && rm_dir.isDirectory()) {
